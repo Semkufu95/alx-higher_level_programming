0b62e96c19351dde4f857d0eb9e557db316f8a2e
semkufu95 <semkufu95@gmail.com>
2024-01-07 04:27:04 -0800

    Adding tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for n in line:
-            print("{:d}".format(n), end=" ")
+            print("{:d}".format(n), end=' ')
         print()
