de9b5612ca90968f22cc1adc3a4c3718e4e27472
semkufu95 <semkufu95@gmail.com>
2024-01-09 21:32:04 -0800

    sets of common element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = list(map(lambda x: x.replace(search, replace), my_list))
-    print(my_list)
+    new_list = [replace if x == search else x for x in my_list]
+    return new_list
