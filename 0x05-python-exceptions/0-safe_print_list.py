5055962761eb2423e87322e4a80b8553d52c9a7a
semkufu95 <semkufu95@gmail.com>
2024-02-01 04:50:50 +0300

    printong list by exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,10 @@
 
 def safe_print_list(my_list = [], x = 0):
     item = 0
-    for n in range(x):
-        print(my_list[n], end=' ')
-        item += 1
+    try:
+        for n in range(x):
+            print(my_list[n], end=' ')
+            item += 1
     except IndexError:
         pass
     print()
