5d847751bea2d2e6a15f9890eeb2819737ac8e3b
semkufu95 <semkufu95@gmail.com>
2024-02-01 18:16:40 +0300

    print list of integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             item += 1
         except (ValueError, TypeError):
             continue
-        print('')
+        print('{d:}'.format(item[x]))
         return item
