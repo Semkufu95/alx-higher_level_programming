f4ce13111914645580e7a2e8606ecd0f421777ea
semkufu95 <semkufu95@gmail.com>
2024-06-09 16:29:48 +0300

    6-square.py white space

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -24,7 +24,7 @@ class Square:
         elif value < 0:
             raise ValueError('size must be >=0')
         self.__size = value
-        
+
     def area(self):
         ''' returns the area of square'''
         return (self.__size * self.__size)
