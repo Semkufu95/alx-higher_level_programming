6d76924024e22809ab0ecce8e85e6d2216f19e37
semkufu95 <semkufu95@gmail.com>
2024-06-09 04:15:09 +0300

    added comments to 3-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+'''import module or define the class'''
+
 
 class Square:
     def __init__(self, size=0):
+        ''' Initialize a new square '''
         if  not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif (size < 0):
