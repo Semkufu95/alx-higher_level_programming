6afe804d3f5105cf2d0a0242748ca6213592ebc4
semkufu95 <semkufu95@gmail.com>
2024-06-19 19:26:05 -0700

    Task 6

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+/*
+A class square inheriting from Previous rectangle
+*/
 
 const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-      super(size, size);
+    super(size, size);
   }
 };
