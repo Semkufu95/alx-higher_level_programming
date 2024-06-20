715430465c23b003b2d314745f93d27eb8fd1024
semkufu95 <semkufu95@gmail.com>
2024-06-19 18:34:21 -0700

    4-rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let m = 0; m < this.height; m++) {
+      let line = '';
+      for (let n = 0; n < this.width; n++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
