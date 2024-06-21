2ccdcb1ea3efaa2c6797673e87f32daf58a6b6cd
semkufu95 <semkufu95@gmail.com>
2024-06-20 19:20:43 -0700

    Correction to task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 const args = process.argv;
 
-if (args[0] === undefined) {
-  console.log('No argument');
-} else {
+if (args[2]) {
   console.log(args[2]);
+} else {
+  console.log('No argument');
 }
