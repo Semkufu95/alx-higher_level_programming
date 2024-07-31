a1d4aa886b129468f0c7296c6f9f44ca15b6ea7d
semkufu95 <semkufu95@gmail.com>
2024-07-31 11:31:23 +0300

    Task 2 correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     item = 0
-    n = 0
-    for n in range(x)
+    for n in range(0, x)
         try:
             print('{:d}'.format(my_list[n]), end='')
             item += 1
