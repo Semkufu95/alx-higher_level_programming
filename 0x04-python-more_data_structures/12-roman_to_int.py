7289fb8c8d8f01afff2227bfa4c9b090cced5ea2
semkufu95 <semkufu95@gmail.com>
2024-07-31 10:33:05 +0300

    correction to task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
-"""
-check if current character index is equal to length
-"I" can be subtracted from "V" and "X",
-"X" can be subtracted from "L" and "C",
-"C" can be subtracted from "D" and "M".
-However, "V", "L", and "D" cannot be used to represent subtraction.
-"""
+
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
     total = 0
-    num = 0
-    if roman_string is not None:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
-        listroman = list(roman.keys())
-        length = len(roman_string)
-        for char in roman_string:
-            # print("++ "+char)
-            if char in roman:
-                # print("+ "+char, roman[char])
-                if char in 'IXC':
-                    if num < length-1:
-                        if listrom[num+1] in 'VXLCDM':
-                            total = total + (roman[listroman[num+1]]-roman[char])
-                total += roman[char]
-            num += 1
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
     return total
