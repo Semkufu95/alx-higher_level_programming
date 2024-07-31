6e6f2483e2a62459dd00d1e51c48031ef4706a6c
semkufu95 <semkufu95@gmail.com>
2024-07-31 09:01:45 +0300

    Correctio to task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 9):
     for j in range(i+1, 10):
-        print("{:02d}, ".format(i*10 + j), end='')
+        middle = ', '
+        if i == 8:
+            middle = '\n'
+        print("{:d}{:d}".format(i, j), end=middle)
