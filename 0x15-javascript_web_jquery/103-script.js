232c9fcac3684e8903a35f0e81f9b7ad433a9b31
semkufu95 <semkufu95@gmail.com>
2024-08-11 20:56:19 +0300

    Task 103

---
+++ 0x15-javascript_web_jquery/103-script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    function fetchTranslation() {
+        const langCode = $('#language_code').val();
+        const apiUrl = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + langCode;
+        
+        $.get(apiUrl, function(data) {
+            $('#hello').text(data.hello);
+        });
+    }
+
+    // Fetch translation on button click
+    $('#btn_translate').click(function() {
+        fetchTranslation();
+    });
+
+    // Fetch translation on pressing ENTER in the input field
+    $('#language_code').keypress(function(event) {
+        if (event.which === 13) { // 13 is the ENTER key
+            fetchTranslation();
+        }
+    });
+});
