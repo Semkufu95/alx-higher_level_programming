4c24a7547891138c06a810b31ae37c80e4677cba
semkufu95 <semkufu95@gmail.com>
2024-08-20 22:27:55 +0300

    initialize class rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A script defining a class rectangle
+"""
+
+class Rectangle:
+    '''
+    initializing a class
+    '''
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains codes that were pushed to solve Alx tasks on more classes and Object Oriented programmng
