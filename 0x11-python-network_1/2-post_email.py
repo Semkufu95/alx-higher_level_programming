3face9347dfb208912707a5b2ecb851b733af482
semkufu95 <semkufu95@gmail.com>
2024-08-22 01:04:50 +0300

    Tasks 5, 6 and 7

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     email = sys.argv[2]
 
     data = urllib.parse.urlencode(email)
-    data = data.encode('ascii') # data should be bytes
+    data = data.encode('ascii')  # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         body = response.read()
