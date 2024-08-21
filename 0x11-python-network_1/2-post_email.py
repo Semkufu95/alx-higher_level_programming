a3bcce0a720166fcf8837070c8b3b4b49444caa4
semkufu95 <semkufu95@gmail.com>
2024-08-22 01:16:40 +0300

    task 8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-''' A script that accepts emal as arg, sends request passed '''
+"""
+    script that takes in a URL and an email,
+    sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+"""
 
-import urllib.request
-import urllib.parse
 import sys
+from urllib import request
+from urllib import parse
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Get the URL and email from the command line argument
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')  # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print('Your email is: {}'.format(body.decode('utf-8')))
+    # Create a dictionary with the email parameter
+    data = {'email': email}
+
+    # Encode the data to be sent in the request body
+    encoded_data = parse.urlencode(data).encode('ascii')
+
+    # Make a POST request to the provided URL with the email parameter
+    with request.urlopen(url, data=encoded_data) as response:
+        # read and decode the response body in utf-8
+        print(response.read().decode('utf-8'))
