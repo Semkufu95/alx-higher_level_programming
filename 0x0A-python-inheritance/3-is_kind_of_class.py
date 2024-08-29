4a7839108eda7e7e4fe9795d0665948413f1eb1b
semkufu95 <semkufu95@gmail.com>
2024-08-29 11:19:40 +0300

    TASK 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+Return kind of class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Return bool for if an object is of an instance'''
+    return isinstance(obj, a_class)
