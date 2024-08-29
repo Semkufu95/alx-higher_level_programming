07239ce59e0059bf1c4cc6e848121623d86e6e83
semkufu95 <semkufu95@gmail.com>
2024-08-29 11:42:49 +0300

    Task 4 Subclasses/

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+Return True if an object is instance of class inherited
+'''
+
+
+def inherits_from(obj, a_class):
+    ''' Check inheritance '''
+    return issubclass(type(obj), a_class)
