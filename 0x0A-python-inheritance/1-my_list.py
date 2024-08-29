e70466a1e178d56178d619a81be9304daf8a0cc7
semkufu95 <semkufu95@gmail.com>
2024-08-29 08:49:02 +0300

    Task 1 my list correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''
-A class MyList that inherits frim list
+A class MyList that inherits from list
 '''
 
 
 class MyList(list):
-'''A class that inherits from a list'''
+    '''A class that inherits from a list'''
     def print_sorted(self):
+        ''' Print a sorted list'''
         print(sorted(self))
